d11a1a6684ce336ad317a5ae5adf49276e405ead
Kormos Máté <kormika004@gmail.com>
2022-11-14 09:30:46 +0100

    restfulapp

---
+++ restfulapp/server/read.php
@@ -0,0 +1,23 @@
+<?php
+// feltételezzük hogy az adatbázisból érkezik:
+ $diakok=[
+    [
+        "nev"=>"Gyula",
+        "szak"=>"Szoftverfejlesztő",
+        "szev"=>2002
+    ],
+    [
+        "nev"=>"Peti",
+        "szak"=>"Művészetek",
+        "szev"=>2001
+    ],
+    [
+        "nev"=>"Anna",
+        "szak"=>"Női szabó",
+        "szev"=>2003
+    ]
+    ];
+
+echo json_encode($diakok);
+
+?>
